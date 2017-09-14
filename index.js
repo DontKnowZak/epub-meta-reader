81cdf93a2cf6ab66c04f6b4ae40ae990c4003e4d
Zak Richardson <zakrc.richardson@gmail.com>
2017-09-14 15:28:44 +0100

    creates directory, writes json to file

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var fs = require("fs");
 var path = require("path");
 var EPub = require("epub");
+var util = require('util');
 var epubFolder = "epubs"
 
 fs.readdir(epubFolder, function(err, files) {
   files.forEach (function(file, index) {
     var epub = new EPub("epubs/" + file);
+
     epub.on("end", function(err){
+      var epubFilename = path.basename(file, '.epub')
+
+      fs.mkdir(epubFilename, (err) => {
+        if (err) throw err;
+        console.log('The folder has been created!');
+      });
+
+      var writePath = epubFilename + "/index.json";
+
       console.log(epub.metadata);
+
+      fs.writeFile(writePath, util.inspect(epub.metadata), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
     });
     epub.parse();
-  })
-})
+  });
+});
