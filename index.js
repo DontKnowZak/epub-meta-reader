1796b4f72dd81f4ba7b4e330e032747e926e8c20
Zak Richardson <zakrc.richardson@gmail.com>
2017-09-14 14:42:21 +0100

    loops through folder 'epubs'

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var fs = require("fs");
+var path = require("path");
 var EPub = require("epub");
 var epub = new EPub("Test Epubs/1.epub");
+var epubFolder = "epubs"
 
+fs.readdir(epubFolder, function(err, files) {
+  files.forEach (function(file, index) {
+    console.log(file);
+  })
+})
 epub.on("end", function(err){
   console.log(epub.metadata);
 });
